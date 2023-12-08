ed4755f90e2a92d66ac47a2d10dd704532702b0d
uweradiane <uweradiane27@gmail.com>
2023-12-07 19:16:22 -0500

    index.php

---
+++ index.php
@@ -0,0 +1,9 @@
+<h1>Mon projet Diane fashion Designs</h1>
+
+<a href="./pages/signup.php">S'enregistrer</a>
+<a href="./pages/login.php">Se connecter</a>
+
+<?php
+session_start();
+var_dump($_SESSION);
+?>
