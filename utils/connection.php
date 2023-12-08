d2767746697be684b8ad746f75044cdcba723b6b
uweradiane <uweradiane27@gmail.com>
2023-12-07 19:18:20 -0500

    validation

---
+++ utils/connection.php
@@ -0,0 +1,14 @@
+<?php
+$server = 'localhost';
+$userName = "root";
+$pwd = "";
+$db = "ecom1_project";
+
+$conn = mysqli_connect($server, $userName, $pwd, $db);
+if ($conn) {
+    //echo "Connected to the $db database successfully";
+    global $conn;
+} else {
+    echo "Error : Not connected to the $db database";
+}
+?>
